0bc9d44972d7ec4dd18453a0687781ee7459b818
naleilan <leilan.n@gmail.com>
2024-01-29 14:31:56 +0300

    add useEffect and useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Question from "./Question";
+
+const initialState = {
+  Questions: [],
+  //'loading', 'error','ready','avtive','finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, Questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Unknown action");
+  }
+}
 
 export default function App() {
-  return <div></div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Question>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Question>
+    </div>
+  );
 }
---
+++ src/Question.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Question({ children }) {
+  return <main className="main">{children}</main>;
+}
+
+export default Question;
