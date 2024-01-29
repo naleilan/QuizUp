a67a20a3edcfbf9145ba8611f30e12a34f331871
naleilan <leilan.n@gmail.com>
2024-01-29 17:00:42 +0300

    193-create options component

---
+++ src/components/App.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useReducer } from "react";
+import Header from "./Header";
+import MainPart from "./MainPart.jsx";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question.jsx";
+
+const initialState = {
+  questions: [],
+  //'loading', 'error','ready','active','finished'
+  status: "loading",
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+export default function App() {
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <MainPart>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
+      </MainPart>
+    </div>
+  );
+}
