f079150bccacfc4340eb3fee9b42472e7ef33c92
naleilan <leilan.n@gmail.com>
2024-01-29 16:43:54 +0300

    193-display questions

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const initialState = {
   questions: [],
   //'loading', 'error','ready','active','finished'
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -26,7 +27,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -46,7 +50,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </MainPart>
     </div>
   );
