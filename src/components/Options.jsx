3c789540328b508b533cf797f8d98f2260365f94
naleilan <leilan.n@gmail.com>
2024-01-30 07:03:56 +0300

    195-add answer

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Options({ question, dispatch, answer }) {
-  console.log(2);
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          className="btn btn-option"
+          className={`btn btn-option ${index === answer ? "answer" : ""}  
+          ${index === question.correctOption ? "correct" : "wrong"} `}
           key={option}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Options from "./Options";
 
 function Question({ question, dispatch, answer }) {
-  console.log(1);
   return (
     <div>
       <h4>{question.question}</h4>
