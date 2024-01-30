0f0e1fc668341a30411f3ae72c66eb4000f154a5
naleilan <leilan.n@gmail.com>
2024-01-30 07:01:02 +0300

    195-add answer

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  console.log(2);
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className="btn btn-option"
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
